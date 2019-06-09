14e4bd221b96a7e6aa6c408850cf988fe6391b28
cakir-enes <cakir-enes@outlook.com>
2019-06-09 16:37:21 +0300

    Models and repo interfaces

---
+++ pkg/user/user.go
@@ -0,0 +1,16 @@
+package user
+
+// User struct
+type User struct {
+	ID       uint   `json:"id"`
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
+
+type Repository interface {
+	FindUserByID(id uint) (*User, error)
+	FindUserByName(name string) (*User, error)
+	SaveUser(user *User) error
+	GetAllUsers() ([]User, error)
+	DeleteUserByID(id uint) error
+}

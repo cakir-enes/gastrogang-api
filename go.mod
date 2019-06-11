4f786272902af3e2d27d420b6ce3b7702697ee11
cakir-enes <cakir-enes@outlook.com>
2019-06-11 21:45:57 +0300

    Updated recipe, user model, repo interfaces

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gastrogang-api
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gin-gonic/gin v1.4.0
+)
--- pkg/user/user.go
+++ pkg/user/user.go
@@ -9,7 +9,6 @@ type User struct {
 
 type Repository interface {
 	FindUserByID(id uint) (*User, error)
-	FindUserByName(name string) (*User, error)
 	SaveUser(user *User) error
 	GetAllUsers() ([]User, error)
 	DeleteUserByID(id uint) error

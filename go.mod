5278131342e806805eebfad082cbe695f5fe9113
cakir-enes <cakir-enes@outlook.com>
2019-06-11 21:51:12 +0300

    Add token to user struct

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module gastrogang-api
 go 1.12
 
 require (
-	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.4.0
 )
--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,13 +1,24 @@
 package user
 
+import "github.com/dgrijalva/jwt-go"
+
 // User struct
 type User struct {
 	ID       uint   `json:"id"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
+	Token    string `json:"token";sql:"-"`
 }
 
-type Repository interface {
+/*
+JWT claims struct
+*/
+type Token struct {
+	UserId uint
+	jwt.StandardClaims
+}
+
+type Service interface {
 	FindUserByID(id uint) (*User, error)
 	SaveUser(user *User) error
 	GetAllUsers() ([]User, error)

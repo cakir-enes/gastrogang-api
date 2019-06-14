a861afbfc911988dcb4d751b459b70c9f9e3fa5d
cakir-enes <cakir-enes@outlook.com>
2019-06-14 20:48:55 +0300

    gcp files, recipe model update

--- pkg/recipe/recipe.go
+++ pkg/recipe/recipe.go
@@ -2,12 +2,12 @@ package recipe
 
 // Recipe model
 type Recipe struct {
-	ID          uint              `json:"id"`
-	Name        string            `json:"name"`
-	Steps       []string          `json:"steps"`
-	Ingredients map[string]string `json:"ingredients"`
-	Details     string            `json:"details"`
-	AuthorID    uint              `json:"authorid"`
+	ID          uint     `json:"id"`
+	Name        string   `json:"name"`
+	Steps       []string `json:"steps"`
+	Ingredients []string `json:"ingredients"`
+	Details     string   `json:"details"`
+	AuthorID    uint     `json:"authorid"`
 }
 
 type Repository interface {
